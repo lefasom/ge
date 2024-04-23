e7d46a0e60496c078278d071081087a28df87bd2
lefasom <leosombra89@gmail.com>
2024-04-22 21:24:42 -0300

    config form

--- angular/src/app/features/login/login.component.ts
+++ angular/src/app/features/login/login.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formulario!: FormGroup;
+  emailControl!: FormControl;
+  passwordControl!: FormControl;
 
+  private _formBuilder = inject(FormBuilder)
+
+  ngOnInit(): void {
+    this.emailControl = new FormControl('');
+    this.passwordControl = new FormControl('');
+    this.formulario = this._formBuilder.group({
+      email: this.emailControl,
+      password: this.passwordControl,
+    });
+  }
+  logIn(): void {
+    console.log(this.formulario.value)
+  }
 }
